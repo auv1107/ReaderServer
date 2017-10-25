7ee34095c975666d9b21484cd118ef49d2b9389f
antiless <antiless@163.com>
2017-10-26 00:59:21 +0800

    Add radio api.
    Signed-off-by: antiless <antiless@163.com>

---
+++ repo/radio.js
@@ -0,0 +1,19 @@
+'use strict';
+var request = require('request');
+
+var handler = function (req, res, next) {
+    var list = [{
+        name: '糗事百科',
+        description: '超搞笑的原创糗事笑话分享社区',
+        sub_title: '热门排行',
+        album_url: 'https://android-artworks.25pp.com/fs08/2017/10/12/11/110_67909199d7f066a11c8c11fbe0be1f85_con.png'
+    }];
+    var result = {
+        data: list,
+        api_type: 'radios',
+        count: list.length
+    }
+    res.json(result);
+};
+
+module.exports = handler;
--- routes/data.js
+++ routes/data.js
@@ -3,5 +3,6 @@ var router = require('express').Router();
 var AV = require('leanengine');
 
 router.get('/qiubai', require('../repo/qiubai'));
+router.get('/radio', require('../repo/radio'));
 
 module.exports = router;
