6c8ab7b10c9dab5108f7d042cca31199c94170bf
antiless <antiless@163.com>
2017-11-01 00:55:40 +0800

    Add code on radio.
    Signed-off-by: antiless <antiless@163.com>

--- repo/radio.js
+++ repo/radio.js
@@ -6,7 +6,8 @@ var handler = function (req, res, next) {
         name: '糗事百科',
         description: '超搞笑的原创糗事笑话分享社区',
         sub_title: '热门排行',
-        album_url: 'https://android-artworks.25pp.com/fs08/2017/10/12/11/110_67909199d7f066a11c8c11fbe0be1f85_con.png'
+        album_url: 'https://android-artworks.25pp.com/fs08/2017/10/12/11/110_67909199d7f066a11c8c11fbe0be1f85_con.png',
+        code: 'qiubai'
     }];
     var result = {
         data: list,
