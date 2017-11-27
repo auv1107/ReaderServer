89a32122097b5a4b463a6bf5b8d99ff5193e2697
lixindong <antiless@163.com>
2017-11-27 13:01:57 +0800

    Add qiubai new.
    Update qiubai hot api.
    
    Signed-off-by: lixindong <antiless@163.com>

--- routes/data.js
+++ routes/data.js
@@ -2,10 +2,11 @@
 var router = require('express').Router();
 var AV = require('leanengine');
 
-router.get('/qiubai', require('../repo/qiubai'));
+router.get('/qiubaihot', require('../repo/qiubaihot'));
 router.get('/zhihu', require('../repo/zhihu'));
 router.get('/scientific_invest', require('../repo/scientific_invest'));
 router.get('/zhidao', require('../repo/zhidao'));
+router.get('/qiubainew', require('../repo/qiubainew'));
 
 router.get('/radio', require('../repo/radio'));
 
